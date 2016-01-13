87402009b9d6a24c6086cc8bdac1cc2f7fb4d1f4
Muhammad Arif Hanif <arif.hanif3@gmail.com>
2016-01-13 01:20:36 -0500

    added redirect testing

--- hello.py
+++ hello.py
@@ -1,20 +1,23 @@
 import os
 
-from flask import Flask, url_for, request
+import flask
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Index Page"
 
 @app.route('/login', methods=['GET', 'POST'])
+
 def login():
-    if request.values and request.method == 'POST':
-        return 'username is ' + request.values["username"]
+    if flask.request.values and flask.request.method == 'POST':
+        return flask.redirect("/")
     else:
         return '<form method="post" action="/login"><input type="text" name="username" /><p><button type="Submit">Submit</button></form>'
 
-    
 if __name__ == '__main__':
-    host = os.getenv('IP', '0.0.0.0')
-    port = int(os.getenv('PORT', 5000))
+
     app.debug = True
-    app.run(host=host, port=port)
+    app.run()
 
